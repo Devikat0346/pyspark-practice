51c035cf11a30c95de04fdc8087c6f6a537af09d
Devika T <135176102+Devikat0346@users.noreply.github.com>
2023-10-09 20:44:05 -0500

    Create pyspark-string-date.py

---
+++ pyspark-string-date.py
@@ -0,0 +1,16 @@
+from pyspark.sql import SparkSession
+
+# Create SparkSession
+spark = SparkSession.builder \
+               .appName('SparkByExamples.com') \
+               .getOrCreate()
+
+from pyspark.sql.functions import *
+
+df=spark.createDataFrame([["02-03-2013"],["05-06-2023"]],["input"])
+df.select(col("input"),to_date(col("input"),"MM-dd-yyyy").alias("date")) \
+  .show()
+
+#SQL
+spark.sql("select to_date('02-03-2013','MM-dd-yyyy') date").show()
+  
